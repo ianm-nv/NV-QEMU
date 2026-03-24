eb3dda3d29bdae79676850d05243059bb8a016bc
Ian May <ianm@nvidia.com>
2026-03-23 22:45:54 -0700

    NVIDIA: SAUCE: hw/arm/virt: Add CCA realm VM type and machine setup
    Add the foundational virt machine support for ARM CCA realms:
    
    - virt_machine_is_confidential() helper to check if the machine
      has a confidential guest support backend
    - virt_kvm_type(): request KVM_VM_TYPE_ARM_REALM and reserve the
      upper GPA bit for realm/NS memory differentiation
    - Use SMC PSCI conduit for realm guests (RMM intercepts SMC as
      RMI_EXIT_PSCI; HVC is not recognized)
    - Call kvm_arm_rme_init_guest_ram() and kvm_arm_rme_init_gpa_space()
      to configure the realm memory layout
    - Add event_log and pci_preserve_config fields to VirtMachineState
    
    Signed-off-by: Ian May <ianm@nvidia.com>

--- include/hw/arm/virt.h
+++ include/hw/arm/virt.h
@@ -180,6 +180,8 @@ struct VirtMachineState {
     bool ns_el2_virt_timer_irq;
     CXLState cxl_devices_state;
     bool legacy_smmuv3_present;
+    bool pci_preserve_config;
+    Object *event_log;
 };
 
 #define VIRT_ECAM_ID(high) (high ? VIRT_HIGH_PCIE_ECAM : VIRT_PCIE_ECAM)
