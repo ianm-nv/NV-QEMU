6c5dbba9ea6b6b4202f3645d45235fa8c9532609
Ian May <ianm@nvidia.com>
2026-03-23 21:54:02 -0700

    NVIDIA: SAUCE: target/arm: Add KVM RME declarations, stubs, and build infrastructure
    Add the scaffolding for ARM CCA (Confidential Computing Architecture)
    realm support:
    
    - Declare all RME function prototypes in kvm_arm.h
    - Add stub implementations in kvm-stub.c for non-KVM builds
    - Add kvm-rme.c to the meson build
    - Make kvm_arm_vcpu_finalize() non-static for use by kvm-rme.c
    - Call kvm_arm_rme_init() from kvm_arch_init() with early-return
      error handling
    - Add KVM_REG_ARM_PMCR_EL0 and KVM_REG_ARM_ID_AA64DFR0_EL1 register
      defines to kvm_arm.h
    - Add kvm_rme flag to ARMCPU struct for per-vCPU realm tracking
    
    Signed-off-by: Ian May <ianm@nvidia.com>

--- target/arm/kvm_arm.h
+++ target/arm/kvm_arm.h
@@ -17,6 +17,9 @@
 #define KVM_ARM_VGIC_V2   (1 << 0)
 #define KVM_ARM_VGIC_V3   (1 << 1)
 
+#define KVM_REG_ARM_ID_AA64DFR0_EL1     ARM64_SYS_REG(3, 0, 0, 5, 0)
+#define KVM_REG_ARM_PMCR_EL0            ARM64_SYS_REG(3, 3, 9, 12, 0)
+
 /**
  * kvm_arm_register_device:
  * @mr: memory region for this device
@@ -152,6 +155,14 @@ void kvm_arm_set_cpu_features_from_host(ARMCPU *cpu);
 void kvm_arm_add_vcpu_properties(ARMCPU *cpu);
 
 /**
+ * @cpu: The CPU object to finalize
+ * @feature: a KVM_ARM_VCPU_* feature
+ *
+ * Finalize the configuration of the given vcpu feature.
+ */
+int kvm_arm_vcpu_finalize(ARMCPU *cpu, int feature);
+
+/**
  * kvm_arm_steal_time_finalize:
  * @cpu: ARMCPU for which to finalize kvm-steal-time
  * @errp: Pointer to Error* for error propagation
@@ -262,5 +273,61 @@ int kvm_arm_set_irq(int cpu, int irqtype, int irq, int level);
 void kvm_arm_enable_mte(Object *cpuobj, Error **errp);
 
 void arm_cpu_kvm_set_irq(void *arm_cpu, int irq, int level);
+
+/**
+ * kvm_arm_rme_init
+ * @ms: the machine state
+ *
+ * Prepare the machine to be a Realm, if the user enabled it.
+ */
+int kvm_arm_rme_init(MachineState *ms);
+
+/**
+ * kvm_arm_rme_vm_type
+ * @ms: the machine state
+ *
+ * Returns the Realm KVM VM type if the user requested a Realm, 0 otherwise.
+ */
+int kvm_arm_rme_vm_type(MachineState *ms);
+
+/**
+ * kvm_arm_rme_vcpu_init
+ * @cs: the CPU
+ *
+ * If the user requested a Realm, setup the given vCPU accordingly. Realm vCPUs
+ * behave a little differently, for example most of their register state is
+ * hidden from the host.
+ */
+int kvm_arm_rme_vcpu_init(CPUState *cs);
+
+/*
+ * kvm_arm_rme_init_guest_ram
+ * @base: base address of RAM
+ * @size: size of RAM
+ *
+ * If the user requested a Realm, set the base and size of guest RAM, in order
+ * to initialize the Realm IPA space.
+ */
+void kvm_arm_rme_init_guest_ram(hwaddr base, size_t size);
+
+/**
+ * kvm_arm_rme_setup_gpa
+ * @highest_gpa: highest address of the lower half of the guest address space
+ * @pci_bus: The main PCI bus, for which PCI queries DMA address spaces
+ *
+ * Setup the guest-physical address space for a Realm. Install a memory region
+ * and notifier to manage the shared upper half of the address space.
+ */
+void kvm_arm_rme_init_gpa_space(hwaddr highest_gpa, PCIBus *pci_bus);
+
+/**
+ * kvm_arm_rme_get_measurement_log
+ *
+ * Obtain the measurement log object if enabled, in order to get its size and
+ * set its base address.
+ *
+ * Returns NULL if measurement log is disabled.
+ */
+Object *kvm_arm_rme_get_measurement_log(void);
 
 #endif
