d7bcf86810bbbed5e988b32603a775003c6701d1
Ian May <ianm@nvidia.com>
2026-03-23 21:54:02 -0700

    NVIDIA: SAUCE: target/arm: Add KVM RME declarations, stubs, and build infrastructure
    Add the scaffolding for ARM CCA (Confidential Computing Architecture)
    realm support:
    
    - Declare all RME function prototypes in kvm_arm.h
    - Add stub implementations in kvm-stub.c for non-KVM builds
    - Add kvm-rme.c to the meson build
    - Make kvm_arm_vcpu_finalize() non-static for use by kvm-rme.c
    - Call kvm_arm_rme_init() from kvm_arch_init() with early-return
      error handling
    - Add KVM_REG_ARM_PMCR_EL0 and KVM_REG_ARM_ID_AA64DFR0_EL1 register
      defines to kvm_arm.h
    - Add kvm_rme flag to ARMCPU struct for per-vCPU realm tracking
    
    Signed-off-by: Ian May <ianm@nvidia.com>

--- target/arm/kvm-stub.c
+++ target/arm/kvm-stub.c
@@ -52,6 +52,20 @@ bool kvm_arm_el2_supported(void)
     return false;
 }
 
+void kvm_arm_rme_init_guest_ram(hwaddr base, size_t size)
+{
+}
+
+void kvm_arm_rme_init_gpa_space(hwaddr highest_gpa,
+                                              PCIBus *pci_bus)
+{
+}
+
+Object *kvm_arm_rme_get_measurement_log(void)
+{
+    return NULL;
+}
+
 /*
  * These functions should never actually be called without KVM support.
  */
@@ -61,6 +75,11 @@ void kvm_arm_set_cpu_features_from_host(ARMCPU *cpu)
 }
 
 void kvm_arm_add_vcpu_properties(ARMCPU *cpu)
+{
+    g_assert_not_reached();
+}
+
+int kvm_arm_vcpu_finalize(ARMCPU *cpu, int feature)
 {
     g_assert_not_reached();
 }
@@ -121,6 +140,21 @@ void kvm_arm_cpu_pre_save(ARMCPU *cpu)
 }
 
 bool kvm_arm_cpu_post_load(ARMCPU *cpu)
+{
+    g_assert_not_reached();
+}
+
+int kvm_arm_rme_init(MachineState *ms)
+{
+    g_assert_not_reached();
+}
+
+int kvm_arm_rme_vm_type(MachineState *ms)
+{
+    g_assert_not_reached();
+}
+
+int kvm_arm_rme_vcpu_init(CPUState *cs)
 {
     g_assert_not_reached();
 }
