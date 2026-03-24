c8d01eab4ab627a9ba5e2ba72aaddb6dc2a2128d
Ian May <ianm@nvidia.com>
2026-03-24 06:29:23 -0700

    NVIDIA: SAUCE: target/arm/kvm-rme: Initialize realm
    Add the realm lifecycle management. The machine calls
    kvm_arm_rme_init() to prepare for launching a Realm, which sets up
    a migration blocker and a VM state change handler. Once the VM
    starts, rme_create_realm() is called to seal and activate the realm.
    
    With the v12 KVM_CAP_ARM_RMI interface, realm creation and activation
    are implicit: the realm is created on first populate and activated on
    first KVM_RUN, so rme_create_realm() currently just marks the guest
    state as protected. Memory population and measurement logging will be
    wired in by subsequent patches.
    
    Also adds kvm_arm_rme_init_guest_ram() to record the guest RAM base
    and size for later use.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Signed-off-by: Ian May <ianm@nvidia.com>

--- target/arm/kvm-rme.c
+++ target/arm/kvm-rme.c
@@ -21,14 +21,25 @@
 #include "qapi/error.h"
 #include "qemu/error-report.h"
 #include "qom/object_interfaces.h"
+#include "migration/blocker.h"
 #include "system/confidential-guest-support.h"
 #include "system/kvm.h"
+#include "system/runstate.h"
 
 #define TYPE_RME_GUEST "rme-guest"
 OBJECT_DECLARE_SIMPLE_TYPE(RmeGuest, RME_GUEST)
 
+typedef struct {
+    hwaddr base;
+    hwaddr size;
+} RmeRamRegion;
+
 struct RmeGuest {
     ConfidentialGuestSupport parent_obj;
+
+    RmeRamRegion init_ram;
+    uint8_t ipa_bits;
+    size_t num_cpus;
 };
 
 OBJECT_DEFINE_SIMPLE_TYPE_WITH_INTERFACES(RmeGuest, rme_guest, RME_GUEST,
@@ -36,6 +47,35 @@ OBJECT_DEFINE_SIMPLE_TYPE_WITH_INTERFACES(RmeGuest, rme_guest, RME_GUEST,
                                           { TYPE_USER_CREATABLE }, { })
 
 static RmeGuest *rme_guest;
+
+/*
+ * Create and prepare the realm.
+ *
+ * v12 flow:
+ *   1. Populate regions via KVM_ARM_RMI_POPULATE ioctl
+ *      (realm is created implicitly on first populate)
+ *   2. Mark guest state as protected
+ *      (realm is activated implicitly on first KVM_RUN)
+ */
+static int rme_create_realm(Error **errp)
+{
+    /* Realm will be activated implicitly on first KVM_RUN */
+    kvm_mark_guest_state_protected();
+    return 0;
+}
+
+static void rme_vm_state_change(void *opaque, bool running, RunState state)
+{
+    Error *err = NULL;
+
+    if (!running) {
+        return;
+    }
+
+    if (rme_create_realm(&err)) {
+        error_propagate_prepend(&error_fatal, err, "RME: ");
+    }
+}
 
 static void rme_guest_class_init(ObjectClass *oc, const void *data)
 {
@@ -57,6 +97,7 @@ static void rme_guest_finalize(Object *obj)
 
 int kvm_arm_rme_init(MachineState *ms)
 {
+    static Error *rme_mig_blocker;
     ConfidentialGuestSupport *cgs = ms->cgs;
 
     if (!rme_guest) {
@@ -72,6 +113,17 @@ int kvm_arm_rme_init(MachineState *ms)
         return -ENODEV;
     }
 
+    rme_guest->num_cpus = ms->smp.max_cpus;
+
+    error_setg(&rme_mig_blocker, "RME: migration is not implemented");
+    migrate_add_blocker(&rme_mig_blocker, &error_fatal);
+
+    /*
+     * The realm activation is done last, when the VM starts, after all images
+     * have been loaded and all vcpus finalized.
+     */
+    qemu_add_vm_change_state_handler(rme_vm_state_change, NULL);
+
     cgs->require_guest_memfd = true;
     cgs->ready = true;
     return 0;
@@ -79,6 +131,10 @@ int kvm_arm_rme_init(MachineState *ms)
 
 void kvm_arm_rme_init_guest_ram(hwaddr base, size_t size)
 {
+    if (rme_guest) {
+        rme_guest->init_ram.base = base;
+        rme_guest->init_ram.size = size;
+    }
 }
 
 int kvm_arm_rme_vcpu_init(CPUState *cs)
